fcb05487e2227602f964b1687a06ab2d3d490ae2
KeitaUenishi <keita.uenishi@gmail.com>
2024-04-16 17:30:31 +0900

    add relation response_no

--- backend/src/app/Http/Controllers/ThreadController.php
+++ backend/src/app/Http/Controllers/ThreadController.php
@@ -16,8 +16,15 @@ class ThreadController extends Controller
      */
     public function index()
     {
-        // Treadのデータをall() で全件取得し、Resourceを使って整形して返す
-        $threads = Thread::all();
+        // withを使用することでリレーション先のデータをまとめて取得することができる
+        // withCountを使用することでリレーション先のデータの件数を取得することができる
+        $threads = Thread::with([
+            'responses' => function ($query) {
+                $query->latest()->limit(10);
+            },
+        ])
+            ->withCount('responses')
+            ->get();
 
         return ThreadWithResponseResource::collection($threads);
     }
@@ -49,7 +56,7 @@ class ThreadController extends Controller
      */
     public function show(int $id)
     {
-        $thread = Thread::findOrFail($id);
+        $thread = Thread::with('responses')->withCount('responses')->findOrFail($id);
 
         return new ThreadWithResponseResource($thread);
     }
